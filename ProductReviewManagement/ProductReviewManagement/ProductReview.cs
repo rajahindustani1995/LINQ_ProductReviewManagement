f49ef40196639e16cdb10b0420f66e2ccdb98de8
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-08-04 17:37:39 +0530

    [Rajesh] Added : Create Product Review Class & List Of Product Review

---
+++ ProductReviewManagement/ProductReviewManagement/ProductReview.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class ProductReview
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool IsLike { get; set; }
+
+        public override string ToString()
+        {
+            return $"ProductID: {ProductID}, UserID: {UserID}, Rating: {Rating}, Review: {Review}, IsLike: {IsLike}";
+        }
+    }
+}
--- ProductReviewManagement/ProductReviewManagement/Program.cs
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -5,7 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Product Review Management");
-
+            List<ProductReview> list = ReviewManagement.AddProductsReview();
+            ReviewManagement.DisplayeProductsReview(list);
+            Console.ReadLine();
         }
 
     }
