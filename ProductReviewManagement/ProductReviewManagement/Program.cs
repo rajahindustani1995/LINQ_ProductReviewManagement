710d09ab612afd1ed1b1a150d52ec462094105e3
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-08-04 15:41:36 +0530

    [Rajesh] Added : Master Branch Created

---
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductReviewManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management");
+
+        }
+
+    }
+}
